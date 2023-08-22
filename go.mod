748aa2b53d59e2dfb661bd54d62b40a11fbe5a44
pravtz <ederson.ufrgs@gmail.com>
2023-08-22 19:57:46 -0300

    feat: add authentication on directories

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module shareFilesSecurely
 
 go 1.21
+
+require github.com/abbot/go-http-auth v0.4.0
+
+require (
+	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
+	auth "github.com/abbot/go-http-auth"
 	"log"
 	"net/http"
 	"os"
 )
+
+func Secret(user, realm string) string {
+	//simple form as an example
+	//generate password https://unix4lyfe.org/crypt/
+
+	if user == "john" {
+		// password is "hello"
+		return "$1$z/TK7/gF$9.GRY43DWKVvVm8uy13WX0"
+	}
+	return ""
+}
 
 func main() {
 	if len(os.Args) != 3 {
@@ -14,7 +26,12 @@ func main() {
 	}
 	httpDir := os.Args[1]
 	port := os.Args[2]
-	fs := http.FileServer(http.Dir(httpDir))
+
+	authenticator := auth.NewBasicAuthenticator("meuserver.com", Secret)
+	http.HandleFunc("/", authenticator.Wrap(func(w http.ResponseWriter, r *auth.AuthenticatedRequest) {
+		http.FileServer(http.Dir(httpDir)).ServeHTTP(w, &r.Request)
+	}))
+
 	fmt.Printf("server http run in port %s\n", port)
-	log.Fatal(http.ListenAndServe(":"+port, fs))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
